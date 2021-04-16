ca06c3ed9c4390f0b8a3d9840e9e1f42effeea4a
samudrobintang10 <samudrobintang10@gmail.com>
2021-04-16 16:17:10 +0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './assets/css/style.css';
+
+function App() {
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-6xl">Javascript React</h1>
+    </div>
+  );
+}
+
+export default App;
